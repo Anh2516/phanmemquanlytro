e7644da576414c3c9e01f850b454d248cb4e5764
Anh2516 <anhdt.b24cc009@stu.ptit.edu.vn>
2026-03-27 17:04:09 +0700

    updateData

--- src/Button/Button.tsx
+++
@@ -1,9 +0,0 @@
-function Button() {
-  return (
-    <div>
-      <button>Bấm đi</button>
-    </div>
-  );
-}
-
-export default Button;
--- src/components/test1/Hello.tsx
+++
@@ -1,8 +0,0 @@
-function Hello() {
-    return (
-        <>
-        <h1> Hello chào cậu</h1>
-        </>
-    );
-};
-export default Hello;
